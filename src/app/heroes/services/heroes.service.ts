669191c369e77cfd0bb769d27bba6d2b11edfea9
Gemma Gomez Cases <ggc.gemma@gmail.com>
2024-12-04 15:03:42 +0100

    fin seccion 14

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,29 @@
+import { Hero } from '../interfaces/hero.interface';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
+import { environments } from '../../../environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+
+  private baseUrl = environments.baseUrl;
+
+  constructor(private http: HttpClient) {}
+
+    getHeroes():Observable<Hero[]> {
+      return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
+    }
+
+    getHeroById(id: string): Observable<Hero | undefined> {
+      return this.http.get<Hero>(`${ this.baseUrl }/heroes/${id}`)
+        .pipe(
+          catchError( error => of(undefined) )
+        );
+    }
+
+    getSuggestions(query: string): Observable<Hero[]> {
+      return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&limit=6`)
+    }
+
+}
