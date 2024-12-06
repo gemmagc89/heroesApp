470ca671fa287ed1acacddb4fd45a5e12af62580
Gemma Gomez Cases <ggc.gemma@gmail.com>
2024-12-06 11:18:46 +0100

    fin seccion 15

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Hero } from '../interfaces/hero.interface';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { environments } from '../../../environments/environments';
 
 @Injectable({providedIn: 'root'})
@@ -24,6 +24,23 @@ export class HeroesService {
 
     getSuggestions(query: string): Observable<Hero[]> {
       return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&limit=6`)
+    }
+
+    addHero(hero: Hero): Observable<Hero> {
+      return this.http.post<Hero>(`${ this.baseUrl }/heroes`, hero);
+    }
+
+    updateHero(hero: Hero): Observable<Hero> {
+      if(!hero.id) throw Error('Hero id is required');
+      return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${hero.id}`, hero);
+    }
+
+    deleteHeroById(id: string): Observable<boolean> {
+      return this.http.delete(`${ this.baseUrl }/heroes/${id}`)
+        .pipe(
+          catchError( err => of(false) ),
+          map( resp => true)
+        );
     }
 
 }
